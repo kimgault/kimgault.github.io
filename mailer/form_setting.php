9d52119dc08f735b0ebc8ed5b6a88acaf0a1bac5
kimgault <kimmygault@gmail.com>
2020-12-19 15:20:23 -0800

    portfolio foundations

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from kimgault.com"; //letter subject
+	$to = 'kimmygault@gmail.com'; // Enter Your E-mail
+	$from = 'admin@donotrespond.com'; // Admin e-mail
+	$fromName = 'Portfolio Site'; // Your company name
+	$charset = 'UTF-8';
+?>
